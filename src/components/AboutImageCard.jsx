6a60e32b8a203dd965014c2584db79fa29370691
sh8941 <zshamoonhaider@gmail.com>
2026-09-07 23:09:59 +0530

    logo removed and format improved

--- src/components/AboutImageCard.jsx
+++ src/components/AboutImageCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import img from '../assets/profile_pic.png'
+import img from '../assets/profile_pic.webp'
 
 const AboutImageCard = () => {
     return (
-        <div className='h-full w-full md:w-1/2 flex justify-center items-center'>
+        <div className="h-full w-full md:w-1/2 flex justify-center items-center">
             <img
                 src={img}
                 alt=""
-                className='h-96 rounded-full border-blue-200 border-8'
+                className="h-72 w-72 md:h-96 md:w-96 aspect-square object-cover rounded-full border-blue-200 border-8"
             />
         </div>
     );
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import img from '../assets/profile_pic.png'
 import AboutTextCard from './AboutTextCard';
 import AboutImageCard from './AboutImageCard';
 
